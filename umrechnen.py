96059742c2c0f15c22c271dd19731d8e7112add6
Florian88123 <97448878+Florian88123@users.noreply.github.com>
2022-01-10 14:36:06 +0100

    Update umrechnen.py

--- umrechnen.py
+++ umrechnen.py
@@ -1,5 +1,8 @@
 #Zuerst groesse definieren zu dem was man eingibt
 groesse = float(input("gib deine Grösse in cm ein: "))
+
+#Dann groesseinfuss definieren zu was man eingegeben hat mal 0.032808
 groesseinfuss = groesse*0.032808
 
+#Dann etwas machen damit die Lösung ausgegeben wird.
 print("sie sind " + str(groesseinfuss) + "fuss gross.")
